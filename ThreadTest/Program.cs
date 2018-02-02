2d0f9f8f8838768fc75da168098e001457be23a3
ScottyMacDev <smcintosh@savers.com>
2018-02-02 15:17:24 -0800

    Initial check in to github

---
+++ ThreadTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ThreadObject> threadObjects = new List<ThreadObject>();
+
+            for(int x=1; x<=10; x++)
+            {
+                threadObjects.Add(new ThreadObject(x, x*2));
+            }
+
+            while(threadObjects.Any(to => to.IsRunning))
+            {
+                Console.WriteLine("Main thread sleeping for 1 second");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
---
+++ ThreadTest/ThreadObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    public class ThreadObject
+    {
+        public bool IsRunning { get; protected set; }
+        protected Thread ProcessThread { get; set; }
+        public int Number { get; private set; }
+        public int SleepSeconds { get; private set; }
+
+        public ThreadObject(int num, int sleepTime)
+        {
+            Number = num;
+            SleepSeconds = sleepTime;
+            ProcessThread = new Thread(new ThreadStart(Processing));
+            IsRunning = true;
+            ProcessThread.Start();
+        }
+
+        private void Processing()
+        {
+            Console.WriteLine($"Thread {Number} Started. Will sleep for {SleepSeconds} and then exit.");
+            Thread.Sleep(TimeSpan.FromSeconds(SleepSeconds));
+            Console.WriteLine($"Thread {Number} finished and exiting.");
+            IsRunning = false;
+        }
+    }
+}
