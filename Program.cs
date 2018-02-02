b5cf2530e04466a4fe2a164bb7114815e9cc0514
ScottyMacDev <smcintosh@savers.com>
2018-02-02 14:10:20 -0800

    Initial check in to source control

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SemaphoreTest
@@ -10,6 +11,31 @@ namespace SemaphoreTest
     {
         static void Main(string[] args)
         {
+            Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "PrinterApp");
+            Printer printerObject = new Printer();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                int j = i;
+                Task.Factory.StartNew(() =>
+                {
+                    semaphoreObject.WaitOne();
+                    printerObject.Print(j);
+                    semaphoreObject.Release();
+                });
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    class Printer
+    {
+        public void Print(int documentToPrint)
+        {
+            Console.WriteLine("Printing document: " + documentToPrint);
+            //code to print document
+            Thread.Sleep(TimeSpan.FromSeconds(1));
         }
     }
 }
